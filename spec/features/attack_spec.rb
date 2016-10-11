1a2558bfe51428dd6a492500163f110ca1ace7e9
James <bakerje@mac.com>
2016-10-11 16:59:38 +0100

    Implemented hit points and related rspec tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,4 +5,12 @@ feature 'Attacking' do
     expect(page).to have_content 'Dave attacked Mittens'
   end
 
+  scenario 'reduce hit points by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
+
 end
