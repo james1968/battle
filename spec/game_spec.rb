3f0ad72d2164b7927cd76abb5f1722e6438b8975
James <bakerje@mac.com>
2016-10-12 11:17:40 +0100

    Created Game class and rspec tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(dave, mittens) }
   let(:dave) { double :player }
   let(:mittens) { double :player }
 
@@ -11,4 +11,17 @@ describe Game do
       game.attack(mittens)
     end
   end
+
+  describe '#player_1' do
+    it 'accetps new player instance' do
+      expect(game.player_1).to eq dave
+    end
+  end
+
+  describe '#player_2' do
+    it 'accetps new player instance' do
+      expect(game.player_2).to eq mittens
+    end
+  end
+  
 end
