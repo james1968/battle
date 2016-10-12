85dbed5a693866c62a85ada93c1b06a32852993a
B-raw <michaelbarry1989@hotmail.com>
2016-10-12 17:17:18 +0100

    Add switch turn test

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -28,3 +28,25 @@ feature 'Attacking' do
     expect(page).to have_content 'Joffrey Lannister HP: 50'
   end
 end
+
+feature 'Switching turns' do
+  context 'seeing the current turn' do
+    scenario 'at the start of the game' do
+      sign_in_and_play
+      expect(page).to have_content "Ned Stark's turn"
+    end
+
+    scenario 'after player 1 attacks' do
+      sign_in_and_play
+      click_button 'Attack'
+      click_link 'Next turn'
+      expect(page).not_to have_content "Ned Stark's turn"
+      expect(page).to have_content "Joffrey Lannister's turn"
+    end
+  end
+
+
+
+
+
+end
