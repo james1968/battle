f19f6e891c5393436028b5dad970745c84c824a5
david anderson <d.a.m.anderson@gmail.com>
2016-10-13 14:52:17 +0100

    Feature test for computer play page

--- spec/features/computer_spec.rb
+++ spec/features/computer_spec.rb
@@ -7,3 +7,10 @@ feature 'Enter names to play with computer' do
     expect(page).to have_content "Play with Computer"
   end
 end
+
+feature 'Player 2 is the computer' do
+  scenario 'after clicking on Play with Computer' do
+    sign_in_and_play_computer
+    expect(page).to have_content "Robot(the Computer)"
+  end
+end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,10 @@ def sign_in_and_play
   fill_in :player_two_name, with: 'Joffrey Lannister'
   click_button 'Submit'
 end
+
+def sign_in_and_play_computer
+  visit('/')
+  fill_in :player_one_name, with: 'Human'
+  fill_in :player_two_name, with: 'Robot'
+  click_button 'Play with Computer'
+end
